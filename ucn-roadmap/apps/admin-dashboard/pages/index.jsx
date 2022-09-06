25d2144f9298bad3058a4961244b68fbf7256bce
Optic-Dex <danielthomsen238@gmail.com>
2022-09-06 14:58:12 +0200

    Root files for dashborad/roadmap

---
+++ ucn-roadmap/apps/admin-dashboard/pages/index.jsx
@@ -0,0 +1,14 @@
+
+
+const Index = ()  => {
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.styled-components file.
+   */
+  return (
+    <h1>Hello from next</h1>
+  );
+}
+
+export default Index;
