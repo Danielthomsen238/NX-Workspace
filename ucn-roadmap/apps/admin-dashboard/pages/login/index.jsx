312c29c952608a7873b1d56df0d920bcb01947cd
Raygud <runigudmundarson@hotmail.com>
2022-09-12 09:40:50 +0200

    Material icons npm

---
+++ ucn-roadmap/apps/admin-dashboard/pages/login/index.jsx
@@ -0,0 +1,17 @@
+const Index = () => {
+
+    return (
+        <>
+            <form action="">
+                <h2>Log in</h2>
+                <input placeholder="Brugernavn" type="text" />
+                <input placeholder="Adgangskode" type="text" />
+
+                <button> Log på </button>
+                <button> Tilmelde </button>
+            </form>
+        </>
+    );
+}
+
+export default Index;
--- ucn-roadmap/apps/roadmap/pages/index.jsx
+++ ucn-roadmap/apps/roadmap/pages/index.jsx
@@ -1,12 +1,7 @@
 
 const Index = () => {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.styled-components file.
-   */
   return (
-   <h1>Hello from next</h1>
+    <h1>Hello from next</h1>
   );
 }
 
